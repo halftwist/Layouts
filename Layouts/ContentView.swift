0ef64546b7740b73d11b62b297559ae39b38aa0f
John Kearon <jkearon@hotmail.com>
2025-03-12 08:43:32 -0400

    Swift lesson 31

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "When the Genius Bar Needs Help, They Call You!"
     
     var body: some View {
         VStack {
+//            Text("You Have Skills!")
+//                .font(.largeTitle)
+//                .fontWeight(.black)
+//                .foregroundStyle(.goldBC)
+//                .padding()
+//                .background(Color("maroon-BC"))
+//                .clipShape(RoundedRectangle(cornerRadius: 0))
+            
+            Spacer()
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -20,13 +29,18 @@ struct ContentView: View {
                 .foregroundStyle(.red)
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
-                .border(Color.gray)
+//                .border(Color.gray)
                 .padding()
 
+            Spacer()
+            Spacer()
+            Spacer()
+            
             HStack {
                 Button("Awesome") {
                     messageString = "You Are Awesome!"
                 }
+                Spacer()
                 Button("Great") {
                     messageString = "You Are Great!"
                 }
@@ -37,6 +51,11 @@ struct ContentView: View {
 //            .border(Color.gray)
         }
         .padding()
+        .background(.yellow.gradient)
+//        .background(
+//            Gradient(colors: [.white, .blue, .red])
+//        )
+//        .background(.yellow.opacity(0.5))
 //        .border(Color.gray)
     }
 }
