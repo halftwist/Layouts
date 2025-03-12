47a11fe2fde4810ba82c67ceb5c9c996df244f4b
John Kearon <jkearon@hotmail.com>
2025-03-12 11:39:55 -0400

    Swift lesson 32

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var messageString = "When the Genius Bar Needs Help, They Call You!"
     
     var body: some View {
+//        GeometryReader { geometry in
+////             container view that defines its content as a function of its own size and coordinate space.
+////            embed the VStack here
+//        }
         VStack {
 //            Text("You Have Skills!")
 //                .font(.largeTitle)
@@ -20,7 +24,16 @@ struct ContentView: View {
 //                .background(Color("maroon-BC"))
 //                .clipShape(RoundedRectangle(cornerRadius: 0))
             
+            Text("Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.indigo)
+            
             Spacer()
+            
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -33,9 +46,18 @@ struct ContentView: View {
                 .padding()
 
             Spacer()
-            Spacer()
-            Spacer()
+//            Spacer()
+//            Spacer()
+            Divider()
+                .background(.black)
+                .padding()
+                .frame(width: 150.0)
             
+            Rectangle()
+                .fill(.indigo)
+                .frame(width: 175, height: 1)
+//                .frame(width: geometry.size.width * 2/3, height: 1)
+
             HStack {
                 Button("Awesome") {
                     messageString = "You Are Awesome!"
@@ -48,10 +70,14 @@ struct ContentView: View {
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .padding()
+            
+            Rectangle()
+                .frame(height: 0)
+                .background(.indigo)
 //            .border(Color.gray)
         }
         .padding()
-        .background(.yellow.gradient)
+//        .background(.yellow.gradient)
 //        .background(
 //            Gradient(colors: [.white, .blue, .red])
 //        )
